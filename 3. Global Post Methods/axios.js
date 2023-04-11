f407f838deb6290361b3c47f15738473600fc5c0
Kenneth Nnabuife <110793854+kennethnnabuife@users.noreply.github.com>
2023-04-11 18:36:15 +0100

    initial commit

---
+++ 3. Global Post Methods/axios.js
@@ -0,0 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// Asynchronous action using Redux Thunk middleware
+export const registerUser = (userData) => async (dispatch) => {
+  dispatch(registerUserStart());
+  try {
+    const response = await axios.post('/api/register', userData);
+    dispatch(registerUserSuccess(response.data));
+    console.log('User registered:', response.data);
+  } catch (error) {
+    dispatch(registerUserFailure(error));
+    console.error('Error registering user:', error);
+  }
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    registerUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    registerUserSuccess: (state, action) => {
+      state.user = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    registerUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+// Export action creators
+export const { registerUserStart, registerUserSuccess, registerUserFailure } = userSlice.actions;
+
+// Export user reducer
+export default userSlice.reducer;
