2beca783a6a5de29f443185b479c2787717d34ca
Kenneth Nnabuife <110793854+kennethnnabuife@users.noreply.github.com>
2023-04-12 16:32:00 +0100

    removed !response.ok

--- 1. Fetch Methods/2. fetch-and-useState.js
+++ 1. Fetch Methods/2. fetch-and-useState.js
@@ -9,9 +9,6 @@ const App = () => {
     const fetchUsers = async () => {
       try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        if (!response.ok) {
-          throw new Error('Failed to fetch users');
-        }
         const data = await response.json();
         setUsers(data);
         setLoading(false);
